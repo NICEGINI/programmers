3fb5ca640704788e2d89116b1ed996e26cd822a8
NICEMANEUL <cndtjq22@gmail.com>
2021-01-29 15:55:31 +0900

    다음큰숫자

--- src/level2/다음큰숫자.java
+++ src/level2/다음큰숫자.java
@@ -6,9 +6,11 @@ package level2;
 public class 다음큰숫자 {
 	static class Solution {
 	    public int solution(int n) {
+	    	// 10진수 이진변환해서 스트링으로 넘겨줌
 	    	String b_num = Integer.toBinaryString(n);
-	    	int onecnt = 0;
+	    	int onecnt = 0; 
 	    	
+	    	// 현재 n의 1의 개수를 카운트
 	    	for(char binary : b_num.toCharArray()) {
 	    		if(binary == '1') {
 	    			onecnt++;
@@ -17,6 +19,7 @@ public class 다음큰숫자 {
 	    	
 	    	int answer = 0;
 	    	
+	    	// n보다 큰 수 중 1의 수가 같은 수를 찾는다
 	    	for(int next = n+1; next <= 10000000; next++) {
 	    		int next_one_cnt = 0;
 	    		String next_binary = Integer.toBinaryString(next);
