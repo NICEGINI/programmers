0b52f41245772194c597470f1b01cb6c98a7a364
NICEMANEUL <cndtjq22@gmail.com>
2021-04-16 00:39:00 +0900

    level3_투포인터

---
+++ src/level3/풍선터트리기.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package level3;
+
+/**
+ * @author YSM
+ *
+ */
+public class 풍선터트리기 {
+	static class Solution {
+	    public int solution(int[] a) {
+	    	int answer = 2; // 처음과 끝은 무조건 남길 수 있다.
+	        int l = a[0], r = a[a.length-1];
+	        for (int i = 1; i < a.length-1; i++) { // 가운데 풍선을 걸러낸다.
+	 		if(l > a[i]) {
+	 			l = a[i]; 
+	 			answer++;
+	 		}
+	 		if(r > a[a.length-1-i]) {
+	 			r = a[a.length-1-i];
+	 			answer++;
+	 		}
+	 	}
+	         return l ==r ? answer-1 : answer; // l과 r이 같으면 중복 발생
+	    }
+	}
+	public static void main(String[] args) {
+		int[] a = {9, -1, -5};
+		System.out.println(new Solution().solution(a));
+	}
+}
