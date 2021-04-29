5b7486d5123f871aee6eef09325d398b180fac61
NICEMANEUL <cndtjq22@gmail.com>
2021-04-29 23:39:54 +0900

    임시

---
+++ src/level2/수식최대화.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package level2;
+
+/**
+ * @author YSM
+ *
+ */
+public class 수식최대화 {
+	static class Solution {
+		private static long MAX;
+
+		public long solution(String expression) {
+			
+			long answer = 0;
+			return answer;
+		}
+	}
+
+	public static void main(String[] args) {
+		String expression = "100-200*300-500+20";
+		System.out.println(new Solution().solution(expression));
+	}
+}
